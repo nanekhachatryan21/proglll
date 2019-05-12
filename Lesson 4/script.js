8ce07c70ca52a1ba6b9e9daa1a9b4e9e104216d3
unknown <Nane Khachatryan>
2019-05-12 17:13:02 +0400

    add files

--- Lesson 4/script.js
+++ Lesson 4/script.js
@@ -36,32 +36,41 @@
 //     console.log(mouseX, mouseY);
 //  }
  
-function main() {
-    var socket = io();
-    var chatDiv = document.getElementById('chat');
-    var input = document.getElementById('message');
-    var button = document.getElementById('submit');
+// function main() {
+//     var socket = io();
+//     var chatDiv = document.getElementById('chat');
+//     var input = document.getElementById('message');
+//     var button = document.getElementById('submit');
  
-    function handleSubmit(evt) {
-        var val = input.value;
-        if (val != "") {
-            socket.emit("send message", val);
-        }
-    }
-    button.onclick = handleSubmit;
+//     function handleSubmit(evt) {
+//         var val = input.value;
+//         if (val != "") {
+//             socket.emit("send message", val);
+//         }
+//     }
+//     button.onclick = handleSubmit;
  
-    function handleMessage(msg) {
-        var p = document.createElement('p');
-        p.innerText = msg;
-        chatDiv.appendChild(p);
-        input.value = "";
+//     function handleMessage(msg) {
+//         var p = document.createElement('p');
+//         p.innerText = msg;
+//         chatDiv.appendChild(p);
+//         input.value = "";
+// }
+
+// socket.on('display message', handleMessage);
+// } // main closing bracket
+
+// window.onload = main;   
+
+function setup (){
+    createCanvas(500, 500);
+    background("#acacac");
 }
-
-socket.on('display message', handleMessage);
-} // main closing bracket
-
-window.onload = main;   
-
+function mouseDragged() {
+    ellipse(mouseX, mouseY, 12, 12);
+    fill("blue");
+    return false;
+  }
  
 
 
